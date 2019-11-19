6299d5a974fbf4155cc5fa10d3d05ef737fd7568
AndrewMRPI <maa5@rpi.edu>
2019-11-19 18:26:40 -0500

    Updated code for new version of docker + all operators supported now.

--- src/main_input.py
+++ src/main_input.py
@@ -1,19 +1,13 @@
-def MAGIC_EQ(x, y):
-	return x == y
-
 def Banana(c):
 	b = c + 7
 	x = 1
 	d = x + 9
 	a = c
-	if (MAGIC_EQ(c, b)):
+	if (c <= b):
 		a = 2 * c
-	elif (MAGIC_EQ(a, c)):
-		a = c + 5
-		d = b + 10
 	else:
 		d = a + 4
-	if (MAGIC_EQ(a, d)):
+	d = d + 3
+	if (a > d):
 		raise("Oh no")
 Banana(10)
-
