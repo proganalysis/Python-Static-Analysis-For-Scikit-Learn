255685f7963290dfe8536cb1d2b5246190d621f5
AndrewMRPI <maa5@rpi.edu>
2019-11-22 15:44:24 -0500

    aa

--- src/main_input.py
+++ src/main_input.py
@@ -1,13 +1,8 @@
 def Banana(c):
-	b = c + 7
-	x = 1
-	d = x + 9
-	a = c
-	if (c <= b):
-		a = 2 * c
-	else:
-		d = a + 4
-	d = d + 3
-	if (a > d):
+	a = "apple"
+	b = "banana"
+	l = [a, b, c]
+	d = "durian"
+	if d in l:
 		raise("Oh no")
-Banana(10)
+Banana("coconut")
